9df4bb67bd8337e504841a807dddd07684ed66be
Arne Kvaleberg <arnekva@gmail.com>
2019-09-28 18:08:34 +0200

    Added support for wage
    Calculates avg monthly payment with salary, and warns you if it dips below 8000 a month

--- WebContent/js/ajaxHandler.js
+++ WebContent/js/ajaxHandler.js
@@ -10,6 +10,53 @@ class ajaxBody{
 }
 
 
+function plotGraf(json){
+  console.log(json[0])
+
+  console.log(json[0].innbetaling)
+  var labels = []
+  var data = []
+  let averagepayment = 0;
+  for(let i = 0; i<json.length;i++){
+    averagepayment += json[i].innbetaling -0
+    labels[i] = json[i].dato
+    data[i] = json[i].restgjeld
+  }
+
+  var ctx = canvas.getContext('2d');
+  var config = {
+     type: 'line',
+     data: {
+        labels: labels,
+        datasets: [{
+           label: 'Resterende gjeld',
+           data: data,
+           borderColor: 'rgba(245, 158, 66, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        }]
+     }
+  };
+  let average = (averagepayment/json.length).toFixed(2)
+  let lengde = json.length
+  printFaktaTilHTML(average, lengde)
+  var chart = new Chart(ctx, config);
+}
+
+function printFaktaTilHTML(average, lengde){
+
+  document.getElementById('averagepayment').innerHTML = "Du vil i gjennomsnitt betale " + average + " kroner per måned."
+  document.getElementById('antaar').innerHTML ="Lånet vil gå over " + (lengde/12).toFixed(2) + " år (" + lengde + " måneder)."
+  let mndlonn = document.getElementById('mndlonn').value
+  let pengerAlevePa = (mndlonn - average).toFixed(2)
+  console.log(mndlonn)
+  console.log("wtf")
+  if(pengerAlevePa < 8000 && mndlonn-0 !== 0){
+    document.getElementById('warningtext').innerHTML = "Månedspriser er sannsynligvis for høy for din inntekt og vil gi deg " + pengerAlevePa + " å bruke i måneden. Forsøk å sette datoen litt lenger bak"
+  }else{
+    document.getElementById('warningtext').innerHTML = ""
+  }
+
+}
 function tegnPunkter(jsonresult) {
 var dataPointsA = []
 var dataPointsB = []
@@ -28,12 +75,12 @@ let averagepayment = 0;
         y: field[i].yyy
       }); */
     }
-    document.getElementById('averagepayment').innerHTML = "Du vil i gjennomsnitt betale " + averagepayment/jsonresult.length + " per måned"
-    document.getElementById('antaar').innerHTML ="Det vil ta " + jsonresult.length + " måneder. (" + jsonresult.length/12 + " år) å betale ned lånet."
+    document.getElementById('averagepayment').innerHTML = "Du vil i gjennomsnitt betale " + (averagepayment/jsonresult.length).toFixed(2) + " kroner per måned."
+    document.getElementById('antaar').innerHTML ="Lånet vil gå over " + (jsonresult.length/12).toFixed(2) + " år (" + jsonresult.length + " måneder)."
 
     var chart = new CanvasJS.Chart("chartContainer", {
       title: {
-        text: "Restbeløp over tid"
+        text: ""
       },
 
       data: [{
@@ -53,7 +100,7 @@ let averagepayment = 0;
 
 
 $(document).ready(function(){ // USE $
-let notpressed = true;
+let notpressed = true
 
   $('.daterange').on('change', function () {
     let daterange = document.getElementById('daterange').value
@@ -69,6 +116,7 @@ let notpressed = true;
       let x =
       document.getElementById('range-span').style.display ='inline'
       document.getElementById('range-span').classList.add('smoother')
+      document.getElementById('faktaboks-right').style.display='inline'
       return false;
 
   });
@@ -79,6 +127,7 @@ function rangeSaveToHidden(){
 function clearHidden(){
   document.getElementById('dateForRange').value= ""
 }
+let ikkeslidet = true
 function dateRangeNearest(daterangevalue, dato){
   let savedDate = new Date(dato)
   savedDate.setYear(savedDate.getFullYear()+(daterangevalue-0))
@@ -88,7 +137,12 @@ function dateRangeNearest(daterangevalue, dato){
     document.getElementById('rangetext').innerHTML = savedDate.getFullYear()
   } else if (daterangevalue === "0"){
     console.log("fooo")
-    document.getElementById('rangetext').innerHTML = "Dra slideren for å endre fullføringsår"
+    if(ikkeslidet){
+    document.getElementById('rangetext').innerHTML = "Dra slideren for å gå opp eller ned noen år"
+    ikkeslidet = false;
+  } else{
+    document.getElementById('rangetext').innerHTML = "Velg ny dato i skjemaet og trykk 'Submit' for å endre strekningen av årstall"
+  }
   }
 
   return datestring
@@ -125,7 +179,7 @@ $.ajax({
               contentType: "application/json; charset=utf-8",
               traditional: true,
               success: function(data) {
-                tegnPunkter(data.nedbetalingsplan.innbetalinger)
+                plotGraf(data.nedbetalingsplan.innbetalinger)
 
               }});
 
